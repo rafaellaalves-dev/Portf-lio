23e3910cfe7ae0a5ac0ffe584c7475512c30446a
Rafaella Alves <rafaellaalves.contato@gmail.com>
2025-10-17 18:01:23 -0300

    Landing page portfolio

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById('toggle-dark').addEventListener('click', function () {
+  document.body.classList.toggle('dark-mode');
+});
+ document.getElementById('abrir-video').addEventListener('click', function () {
+    document.getElementById('video-container').style.display = 'block';
+  });
